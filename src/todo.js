8c764927daf1bb6d546556a1b204f84e25819838
Kinga Mielcarska <mielcarska@gmail.com>
2019-09-14 14:43:29 +0200

    "add new task" button functionality

--- src/todo.js
+++ src/todo.js
@@ -1,12 +1,25 @@
-// Search
+// SEARCH
 
-// Log out
 
-// Dodaj nową listę
+// LOG OUT
 
-// Dodaj nowe zadanie
-function showDiv() {
-    document.getElementsByClassName("add-task").style.display = "block";
-}
 
-// Nowe zadanie - zapisz
+// DODAJ NOWĄ LISTĘ
+
+
+// DODAJ NOWE ZADANIE
+    // pokazuje ukryty div
+$('.add-new-task').click(function() {
+    $('.add-task').toggle("slide");
+});
+    // zmienia wskaźnik na palec (bez tego jest strzałka domyślna mimo cssa)
+$('.add-new-task').hover(function() {
+    $('.add-new-task').css( 'cursor', 'pointer' );
+});
+    // po kliknięciu na dowolny punkt poza "nowym zadaniem" div się ukrywa
+$(".main").click(function (e) {
+    $('.add-task').hide();
+});
+
+
+// NOWE ZADANIE - ZAPISZ
