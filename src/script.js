2a0551f41956c910b97e8a5232ea0dfec8324d5e
Kinga Mielcarska <mielcarska@gmail.com>
2019-09-14 10:38:40 +0200

    Login in JS first test

--- src/script.js
+++ src/script.js
@@ -11,3 +11,35 @@ $('.form button').click(function () {
         opacity: "toggle"
     }, "slow")
 })
+
+// Logowanie:
+const logIn = () => {
+    let loginName = document.getElementById("login-name").value;
+    let loginPassword = document.getElementById("login-password").value;
+    logData(loginName, loginPassword);
+}
+const logData = async (loginName, loginPassword) => {
+    let userData = {
+        "login-name": loginName,
+        "login-password": loginPassword
+    }
+    // Autentykacja do poprawy
+    await fetch('https://herokuapp.com/api/auth', { // Dodać adres!
+        method: "POST",
+        body: JSON.stringify(userData),
+        headers: {
+            "Content-Type": "application/json"
+        }
+    })
+    .then(res => res.text())
+    .then(res =>{console.log(res)
+        if(res=='Invalid email or password'){
+            return alert("Invalid email or password")
+        } 
+        else{
+            var queryString = `?myParam=${res}`;
+            window.location.href = "./todo.html" + queryString;
+        }
+    })
+    .catch(err => console.log(err));
+}
