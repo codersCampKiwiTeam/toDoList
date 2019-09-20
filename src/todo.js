6a5a7d0f31fd117e9c6ba120afd8f76c60803404
Jakub <j_bednarz28.03.92@wp.pl>
2019-09-20 16:24:14 +0200

    Fixing cors

--- src/todo.js
+++ src/todo.js
@@ -34,7 +34,7 @@ async function showTasks() {
     const urlParams = new URLSearchParams(window.location.search);
     const myParam = urlParams.get('myParam');
 
-    await fetch('https://kiwitodoapp.herokuapp.com/tasks', {         // DODAĆ ADRES!
+    await fetch('https://cors-anywhere.herokuapp.com/https://kiwitodoapp.herokuapp.com/tasks', {         // DODAĆ ADRES!
         method: "GET",
         headers: {
             "Content-Type": "application/json",
@@ -105,7 +105,7 @@ async function saveNewTask() {
     const urlParams = new URLSearchParams(window.location.search);
     const myParam = urlParams.get('myParam');
 
-    await fetch('https://kiwitodoapp.herokuapp.com/tasks', { // DODAĆ ADRES!
+    await fetch('https://cors-anywhere.herokuapp.com/https://kiwitodoapp.herokuapp.com/tasks', { // DODAĆ ADRES!
             method: "POST",
             body: JSON.stringify(body),
             headers: {
